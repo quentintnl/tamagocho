bc915f91e93d4ded54aa44dd4fd340116bb5d86e
Ota <q.tonellato@free.fr>
2025-10-28 11:11:28 +0100

    feat: implement monster state update functionality and refactor GET route

--- src/app/api/monster/route.ts
+++ src/app/api/monster/route.ts
@@ -2,8 +2,9 @@ import { connectMongooseToDatabase } from '@/db'
 import Monster from '@/db/models/monster.model'
 import { auth } from '@/lib/auth'
 import { headers } from 'next/headers'
+import { NextRequest } from 'next/server'
 
-export async function GET (): Promise<Response> {
+export async function GET (request: NextRequest): Promise<Response> {
     const session = await auth.api.getSession({
         headers: await headers()
     })
@@ -12,9 +13,14 @@ export async function GET (): Promise<Response> {
         return new Response('Unauthorized', { status: 401 })
     }
 
+    const monsterId = request.nextUrl.searchParams.get('id')
+    if (monsterId === null || monsterId === undefined) {
+        return new Response('Missing monster id', { status: 400 })
+    }
+
     await connectMongooseToDatabase()
-    const monsters = await Monster.find({ ownerId: session.user.id }).exec()
+    const monster = await Monster.findOne({ ownerId: session.user.id, _id: monsterId }).exec()
 
     // Sérialisation JSON pour éviter les problèmes de typage Next.js
-    return Response.json(monsters)
+    return Response.json(monster)
 }
